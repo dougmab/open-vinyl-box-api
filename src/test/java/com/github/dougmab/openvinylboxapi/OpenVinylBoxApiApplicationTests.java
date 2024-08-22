8703ed0917f45e862b537f6faf7beb811fcc2bdc
Douglas Brum <douglas.mateus2005@gmail.com>
2024-08-22 19:16:45 -0300

    initial commit

---
+++ src/test/java/com/github/dougmab/openvinylboxapi/OpenVinylBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dougmab.openvinylboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenVinylBoxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
