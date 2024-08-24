a903d8a38474b8dd2325d6aa99821c7bf602cd79
Douglas Brum <douglas.mateus2005@gmail.com>
2024-08-23 23:17:37 -0300

    feat: add category seed for testdb

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (name) VALUES ('Jazz');
+INSERT INTO category (name) VALUES ('Pop');
+INSERT INTO category (name) VALUES ('Rock');
+INSERT INTO category (name) VALUES ('Chill');
+INSERT INTO category (name) VALUES ('Rap');
+INSERT INTO category (name) VALUES ('Metal');
