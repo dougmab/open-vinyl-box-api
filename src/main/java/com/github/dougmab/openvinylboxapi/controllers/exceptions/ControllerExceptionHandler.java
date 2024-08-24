e753f1643c3ac5f554d5dcb2c3c45cdb0a981217
Douglas Brum <douglas.mateus2005@gmail.com>
2024-08-24 12:37:33 -0300

    feat: add exception handling

--- src/main/java/com/github/dougmab/openvinylboxapi/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/github/dougmab/openvinylboxapi/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.github.dougmab.openvinylboxapi.controllers.exceptions;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+        StandardError err = new StandardError(e, request);
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("Resource not found");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
 }
