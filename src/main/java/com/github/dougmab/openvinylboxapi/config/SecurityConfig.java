df25c039d1a4edbf123ee1782244375f6fa30712
Douglas Brum <douglas.mateus2005@gmail.com>
2024-09-03 13:17:01 -0300

    feat: add spring security

--- src/main/java/com/github/dougmab/openvinylboxapi/config/SecurityConfig.java
+++ src/main/java/com/github/dougmab/openvinylboxapi/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.github.dougmab.openvinylboxapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpRequest) throws Exception {
+        // Provisory configuration
+        httpRequest.authorizeHttpRequests(request ->
+                request.anyRequest().permitAll()
+        ).headers(headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+        ).csrf(AbstractHttpConfigurer::disable);
+
+
+        return httpRequest.build();
+    }
 }
